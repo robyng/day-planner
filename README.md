d6c3299a70b3e4313f691e927ffc7486c7022d21
robyng <robyngraham01@gmail.com>
2021-10-27 16:48:20 -0700

    add screenshot to images for readme preview

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Work Day Scheduler 
-For help with scheduling out your work day. Hours 9am - 5pm enter a task for each hour and press save.
+Use this app for help with scheduling out your work day. Hours 9am - 5pm enter a task for each hour and press save.
 
 ## Links
 deployed at: https://robyng.github.io/day-planner/
 
 repository at: https://github.com/robyng/day-planner 
 
+## Preview
+![App Screenshot](./assets/images/preview.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,45 @@ time10.val(localStorage.getItem("10") || "")
 time10.siblings(".saveBtn").on("click", function(){
     localStorage.setItem("10", time10.val())
 })
+
+var time11 = $("#11")
+time11.val(localStorage.getItem("11") || "")
+time11.siblings(".saveBtn").on("click", function(){
+    localStorage.setItem("11", time11.val())
+})
+
+var time12 = $("#12")
+time12.val(localStorage.getItem("12") || "")
+time12.siblings(".saveBtn").on("click", function(){
+    localStorage.setItem("12", time12.val())
+})
+
+var time13 = $("#13")
+time13.val(localStorage.getItem("13") || "")
+time13.siblings(".saveBtn").on("click", function(){
+    localStorage.setItem("13", time13.val())
+})
+
+var time14 = $("#14")
+time14.val(localStorage.getItem("14") || "")
+time14.siblings(".saveBtn").on("click", function(){
+    localStorage.setItem("14", time14.val())
+})
+
+var time15 = $("#15")
+time15.val(localStorage.getItem("15") || "")
+time15.siblings(".saveBtn").on("click", function(){
+    localStorage.setItem("15", time15.val())
+})
+
+var time16 = $("#16")
+time16.val(localStorage.getItem("16") || "")
+time16.siblings(".saveBtn").on("click", function(){
+    localStorage.setItem("16", time16.val())
+})
+
+var time17 = $("#17")
+time17.val(localStorage.getItem("17") || "")
+time17.siblings(".saveBtn").on("click", function(){
+    localStorage.setItem("17", time17.val())
+})
