18629f54deae9e674c980fc4c34665828513207e
robyng <robyngraham01@gmail.com>
2021-10-27 16:22:20 -0700

    add all time rows in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,9 @@ time9.val(localStorage.getItem("9") || "")
 time9.siblings(".saveBtn").on("click", function(){
     localStorage.setItem("9", time9.val())
 })
+
+var time10 = $("#10")
+time10.val(localStorage.getItem("10") || "")
+time10.siblings(".saveBtn").on("click", function(){
+    localStorage.setItem("10", time10.val())
+})
