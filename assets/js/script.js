9b061590d143cf1fb23f9c7dbd1d25b7e13ad9bb
robyng <robyngraham01@gmail.com>
2021-10-28 11:42:31 -0700

    for loop for each hour element, add present and past if statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@ var date = document.querySelector("#currentDay")
 
 date.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
 
-
+//var hourEl = document.querySelector('#')
+//currentHour = moment().format('h');
 
 
 var time9 = $("#9")
@@ -58,3 +59,48 @@ time17.val(localStorage.getItem("17") || "")
 time17.siblings(".saveBtn").on("click", function(){
     localStorage.setItem("17", time17.val())
 })
+
+var rightNow = moment().format("kk");
+
+
+// var nextHour = moment().add(1, "hour").format("kk");
+// var pastDate = moment("12-01-1999", "MM-DD-YYYY").format("dddd, MM/DD/YY");
+
+var hourEl09 = document.getElementById("9")
+var hourEl10 = document.getElementById("10")
+var hourEl11 = document.getElementById("11")
+var hourEl12 = document.getElementById("12")
+var hourEl13 = document.getElementById("13")
+var hourEl14 = document.getElementById("14")
+var hourEl15 = document.getElementById("15")
+var hourEl16 = document.getElementById("16")
+var hourEl17 = document.getElementById("17")
+//hourElText = moment(hourEl.textContent)
+
+
+var allTimes = [hourEl09, hourEl10, hourEl11, hourEl12, hourEl13, hourEl14, hourEl15, hourEl16, hourEl17 ]
+
+
+for (i = 0; i < allTimes.length; i++) {
+    
+
+    eachTime = allTimes[i]
+
+    if (eachTime.textContent == rightNow) {
+        //console.log(true)
+        $(eachTime).addClass("present")
+    
+    // } else if (eachTime.isAfter(rightNow) == true ){
+    //     $(eachTime).addClass("future")
+    } else {
+        $(eachTime).addClass("past")
+    }
+    
+
+
+
+
+
+}
+
+//moment('2021-10-28 17').isAfter('2021-10-28 09')
